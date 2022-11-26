2d23f74f9c4a80103b5eafee4f4b8c0ddaadc5ba
Phan Ngoc Hoang Long <longpnh2001@gmail.com>
2022-11-26 13:53:47 +0700

    Test purpose

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,15 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  useTheme,
+  Grid,
+} from "@mui/material";
 import { tokens } from "../../theme";
-import { mockTransactions } from "../../data/mockData";
+import { mockTransactions, mockTrendingFood } from "../../data/mockData";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import EmailIcon from "@mui/icons-material/Email";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
@@ -12,10 +19,26 @@ import GeographyChart from "../../components/GeographyChart";
 import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
+import axios from "axios";
+import * as React from "react";
+import { BarItem } from "@nivo/bar";
 
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [newCustomers, setNewCustomers] = React.useState("");
+
+  const fetchData = async () => {
+    let response = await axios.get(
+      `https://localhost:7246/api/Dashboard/Customers`
+    );
+    setNewCustomers(response.data["data"]);
+    console.log(newCustomers);
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Box m="20px">
@@ -23,20 +46,7 @@ const Dashboard = () => {
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
-        <Box>
-          <Button
-            sx={{
-              backgroundColor: colors.blueAccent[700],
-              color: colors.grey[100],
-              fontSize: "14px",
-              fontWeight: "bold",
-              padding: "10px 20px",
-            }}
-          >
-            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
-            Download Reports
-          </Button>
-        </Box>
+        <Box></Box>
       </Box>
 
       {/* GRID & CHARTS */}
@@ -55,26 +65,7 @@ const Dashboard = () => {
           justifyContent="center"
         >
           <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
-            progress="0.75"
-            increase="+14%"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="431,225"
+            title="431,225 VND"
             subtitle="Sales Obtained"
             progress="0.50"
             increase="+21%"
@@ -92,11 +83,22 @@ const Dashboard = () => {
           alignItems="center"
           justifyContent="center"
         >
-          <StatBox
-            title="32,441"
-            subtitle="New Clients"
+          {/* <StatBox
+            title={newCustomers.customers}
+            subtitle="New Customers"
             progress="0.30"
-            increase="+5%"
+            increase={"+" + newCustomers.increase}
+            icon={
+              <PersonAddIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          /> */}
+          <StatBox
+            title="251"
+            subtitle="New Customers"
+            progress="0.30"
+            increase="+15%"
             icon={
               <PersonAddIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
@@ -105,6 +107,25 @@ const Dashboard = () => {
           />
         </Box>
         <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="40"
+            subtitle="Food"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <FastfoodIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        {/* <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
@@ -122,7 +143,7 @@ const Dashboard = () => {
               />
             }
           />
-        </Box>
+        </Box> */}
 
         {/* ROW 2 */}
         <Box
@@ -150,7 +171,7 @@ const Dashboard = () => {
                 fontWeight="bold"
                 color={colors.greenAccent[500]}
               >
-                $59,342.32
+                340,514,000 VND
               </Typography>
             </Box>
             <Box>
@@ -180,6 +201,59 @@ const Dashboard = () => {
             p="15px"
           >
             <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+              Monthly Trending Food
+            </Typography>
+          </Box>
+          {mockTrendingFood.map((food, i) => (
+            <Box
+              key={`${food.no}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box gridColumn="span 4">
+                <Typography variant="h6" color={colors.grey[100]}>
+                  {food.no}
+                </Typography>
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {food.name}
+                </Typography>
+              </Box>
+              {/* <Box color={colors.grey[100]}>{food.quantity}x</Box> */}
+              <Box
+                backgroundColor={colors.greenAccent[500]}
+                p="5px 10px"
+                borderRadius="4px"
+              >
+                Order {food.quantity}x
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 12"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            xs={5} md={5}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            colors={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
               Recent Transactions
             </Typography>
           </Box>
@@ -192,7 +266,11 @@ const Dashboard = () => {
               borderBottom={`4px solid ${colors.primary[500]}`}
               p="15px"
             >
-              <Box>
+              <Box
+                sx={{
+                  width: 200,
+                }}
+              >
                 <Typography
                   color={colors.greenAccent[500]}
                   variant="h5"
@@ -204,46 +282,41 @@ const Dashboard = () => {
                   {transaction.user}
                 </Typography>
               </Box>
-              <Box color={colors.grey[100]}>{transaction.date}</Box>
+              <Box
+                sx={{
+                  width: 100,
+                }}
+                color={colors.grey[100]}
+              >
+                {transaction.date}
+              </Box>
+              <Box
+                sx={{
+                  width: 120,
+                }}
+                color={colors.grey[100]}
+              >
+                {transaction.tableType}
+              </Box>
+              <Box
+                sx={{
+                  width: 100,
+                }}
+                color={colors.grey[100]}
+              >
+                {transaction.numOfPeople}
+              </Box>
               <Box
                 backgroundColor={colors.greenAccent[500]}
                 p="5px 10px"
                 borderRadius="4px"
               >
-                ${transaction.cost}
+                {transaction.cost} VND
               </Box>
             </Box>
           ))}
         </Box>
-
-        {/* ROW 3 */}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
-          >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              $48,352 revenue generated
-            </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box>
-        <Box
+        {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -275,7 +348,7 @@ const Dashboard = () => {
           <Box height="200px">
             <GeographyChart isDashboard={true} />
           </Box>
-        </Box>
+        </Box> */}
       </Box>
     </Box>
   );
